c40fbafd8ccba3c4f96a39a21636804a663bfb17
jloman <jloman200@gmail.com>
2023-05-05 15:30:03 +0300

    Add Registration form

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,7 +1,80 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div> Register page </div>;
-};
+  return (
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-green-600 lg:max-w-xl">
+        <h3 className="text-2xl font-semibold text-center text-green-500 underline uppercase decoration-wavy">
+          Registration form
+        </h3>
+        <form className="mt-6">
+          <div className="mb-2">
+            <label
+              for="username"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              for="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              for="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              for="cpassword"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
+              Register Now
+            </button>
+          </div>
+        </form>
 
+        <p className="mt-8 text-xs font-light text-center text-gray-700">
+          {" "}Account already exist please login?{" "}
+          <Link
+            to="login"
+            className="font-medium text-green-600 hover:underline"
+          >
+            Login here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default Register;
