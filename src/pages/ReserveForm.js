aa54aa15b3ffe390947a53ade1079e6c5cce514c
jloman <jloman200@gmail.com>
2023-05-05 16:12:13 +0300

    Add Reservation form

--- src/pages/ReserveForm.js
+++ src/pages/ReserveForm.js
@@ -1,7 +1,68 @@
 import React from "react";
 
 const ReserveForm = () => {
-  return <div>Reserve form</div>;
+  return (
+    <form className="w-50 mx-auto">
+      <div className="relative w-full">
+        <h2 className="text-center text-2xl text-green-700 underline">
+          Reservation Form
+        </h2>
+      </div>
+
+      <div className="relative z-0 w-full mb-6 group">
+        <label
+          for="floating_email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          State
+        </label>
+        <div className="relative">
+          <select
+            name="floating_email"
+            id="floating_email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          >
+            <option>New Mexico</option>
+            <option>Missouri</option>
+            <option>Texas</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="relative z-0 w-full mb-6 group">
+        <input
+          type="date"
+          name="floating_email"
+          id="floating_email"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          for="floating_email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Date
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      >
+        Reserve Now
+      </button>
+    </form>
+  );
 };
 
 export default ReserveForm;
