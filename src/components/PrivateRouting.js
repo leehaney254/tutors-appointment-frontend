6da39e7030a2b8eebc9d4b7b58cbb5b3d9101605
Leehaney gee <georgeleehaney20@gmail.com>
2023-05-15 16:08:15 +0300

    Feat:update private routing

--- src/components/PrivateRouting.js
+++ src/components/PrivateRouting.js
@@ -1,10 +1,10 @@
 import { Navigate } from 'react-router-dom';
-import ValidateToken from './ValidateToken';
+// import ValidateToken from './ValidateToken';
 
 const PrivateRouting = ({ children }) => {
   const token = window.localStorage.getItem('token');
-  const isValidToken = ValidateToken(token);
-  return token && isValidToken ? children : <Navigate to="/login" />;
+  // const isValidToken = ValidateToken(token);
+  return token ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRouting;
