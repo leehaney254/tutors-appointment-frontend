7fb8f070154f6dbcd4a508832f83058f2b960546
Leehaney gee <georgeleehaney20@gmail.com>
2023-05-15 16:10:44 +0300

    Feat: Update redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from '../features/RegisterSlice';
 import loginReducer from '../features/LoginSlice';
+import tutorReducer from '../features/TutorSlice';
 
 export default configureStore({
   reducer: {
     register: registerReducer,
     login: loginReducer,
+    tutor: tutorReducer,
   },
 });
