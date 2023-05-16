924a3fcfaf7e2f41f89673f57fa8cf7d88a14bd6
Josphat205 <jloman200@gmail.com>
2023-05-16 12:57:01 +0300

    Updates

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from '../features/RegisterSlice';
 import loginReducer from '../features/LoginSlice';
 import tutorReducer from '../features/TutorSlice';
+import reservedTutorSlice from '../features/ReservationSlice';
 
 export default configureStore({
   reducer: {
     register: registerReducer,
     login: loginReducer,
     tutor: tutorReducer,
+    reservedTutor: reservedTutorSlice,
   },
 });
