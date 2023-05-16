c3aa74532f41bd41062c2e3f123ba95c18cf399b
codehass <hassan.elouardy06@gmail.com>
2023-05-16 09:45:09 +0000

    Edit Card feature

--- src/components/CardFeature.js
+++ src/components/CardFeature.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+// import getTutor from '../features/SingleTutorSlice';
+// import { useDispatch, useSelector } from 'react-redux';
 
 const CardFeature = ({
   // eslint-disable-next-line react/prop-types
@@ -10,27 +12,20 @@ const CardFeature = ({
   price,
   // eslint-disable-next-line react/prop-types
   image,
+  id,
 }) => (
   <div className="flex flex-col justify-center  min-h-[260px] min-w-[220px]  p-1 bg-white border border-gray-200 drop-shadow-full hover:shadow-lg rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 scale-100">
-    <Link to="/">
+    <Link to={`/tutors/${id}`}>
       <div className="h-48 w-[13rem] mb-6 flex justify-center">
         <img src={image} alt="" className="w-full h-full" />
       </div>
-      <p className=" text-green-600 font-bold text-lg capitalize">
-        {name}
-      </p>
-      <p className=" text-slate-600 font-medium text-sm">
-        {speciality}
-      </p>
+      <p className=" text-green-600 font-bold text-lg capitalize">{name}</p>
+      <p className=" text-slate-600 font-medium text-sm">{speciality}</p>
       <p className=" text-green-700 font-bold text-xs">
         KES
-        <span className="text-black">
-          {' '}
-          {price}
-        </span>
+        <span className="text-black"> {price}</span>
       </p>
     </Link>
   </div>
 );
-
 export default CardFeature;
