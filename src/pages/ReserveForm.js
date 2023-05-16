a69d9c87fed673889a50f594f09e1b51f9b79cc8
Josphat205 <jloman200@gmail.com>
2023-05-16 12:56:09 +0300

    Add reserve tutor form and dispatch actions

--- src/pages/ReserveForm.js
+++ src/pages/ReserveForm.js
@@ -1,52 +1,99 @@
-const ReserveForm = () => (
-  <form>
-    <div className="mb-6">
-      <label
-        htmlFor="email"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import toast from 'react-hot-toast';
+import { reserveTutor } from '../features/ReservationSlice';
+
+const ReserveForm = () => {
+  const tutors = useSelector((state) => state.tutor.data);
+  const username = window.localStorage.getItem('name');
+  const userId = window.localStorage.getItem('userId');
+  const [data, setData] = useState({
+    tutor_id: '',
+    date: '',
+  });
+  const dispatch = useDispatch();
+  const [user_id, setUname] = useState(userId);
+  const { date, tutor_id } = data;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fData = { tutor_id, user_id, date };
+    dispatch(reserveTutor(fData));
+    toast.success('Tutor reserved Successfully');
+    setData(
+      {
+        tutor_id: '',
+        date: '',
+      },
+    );
+    setUname('');
+  };
+  const newDate = new Date().toISOString().split('T')[0];
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-6">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Tutor Name
+        </label>
+        <select name="tutor_id" onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 dark:shadow-sm-light">
+          <option value="1">Select Tutor</option>
+          {
+          tutors.map((val) => (
+            <option key={val.id} value={val.id}>{val.name}</option>
+          ))
+         }
+        </select>
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="username"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          required
+          readOnly
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="date"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Booking Date
+        </label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          min={newDate}
+          onChange={handleChange}
+          value={date}
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 dark:shadow-sm-light"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
       >
-        Tutor Name
-      </label>
-      <select className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 dark:shadow-sm-light">
-        <option value="1">Select Tutor</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-      </select>
-    </div>
-    <div className="mb-6">
-      <label
-        htmlFor="username"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >
-        Username
-      </label>
-      <input
-        type="text"
-        id="username"
-        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-        required
-      />
-    </div>
-    <div className="mb-6">
-      <label
-        htmlFor="date"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >
-        Booking Date
-      </label>
-      <input
-        type="date"
-        id="date"
-        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 dark:shadow-sm-light"
-        required
-      />
-    </div>
-    <button
-      type="submit"
-      className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-    >
-      Book Appointment
-    </button>
-  </form>
-);
+        Book Appointment
+      </button>
+    </form>
+  );
+};
 export default ReserveForm;
