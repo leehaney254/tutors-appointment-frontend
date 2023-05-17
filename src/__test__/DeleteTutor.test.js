0ff7251f0d9c1d52db1dea70cc3b64c7340d027d
codehass <hassan.elouardy06@gmail.com>
2023-05-17 14:40:07 +0000

    Add test for DeleteTutor

---
+++ src/__test__/DeleteTutor.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../app/store';
+import DeleteTutor from '../pages/DeleteTutor';
+
+it('should render AddTutor page', () => {
+  const deleteTutor = render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <DeleteTutor />
+      </Provider>
+    </BrowserRouter>
+  );
+  expect(deleteTutor).toMatchSnapshot();
+});
